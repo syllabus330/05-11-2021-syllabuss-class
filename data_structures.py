4f1a9b63ca89dd8fea18bf705ea40280e900d964
alexander febres <alexanderfebresth@gmail.com>
2021-11-05 07:29:52 -0500

    initial commit

---
+++ data_structures.py
@@ -0,0 +1,45 @@
+""" DATA TYPES : TIPOS DE DATOS """
+
+# INTEGERS : ENTEROS
+my_integer_number = 20
+
+
+# DECIMALS (Floats numbers) : DECIMALES
+my_decimal_number = 20.25
+
+
+# STRINGS : CADENAS DE TEXTO
+my_string_variable = "Hello Syllabus !"
+
+
+# BOOLEANS : BOOLEANO
+is_today_a_good_day = True
+
+
+""" DATA STRUCTURES : ESTRUCTURAS DE DATOS """
+
+# LISTS : LISTAS
+my_list = [
+    'Hello Syllabus !',
+    20,
+    False,
+    50.55,
+    {'dictionary_key': 'dictionary_value'},
+    ['Hello', 'Syllabus']
+]
+
+
+# DICTIONARIES : DICCIONARIOS
+my_dictionary = {
+    'first_key': 'first_value',
+    'second_key': 'second_value',
+    'third:key': 'third_value'
+}
+
+
+# SETS : SETS
+my_set = (
+    77,
+    'Hello World !',
+    ['Apple', 'Orange', ('set', 'inside list', 'inside set')]
+)
